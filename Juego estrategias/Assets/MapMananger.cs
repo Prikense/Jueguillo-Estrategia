2c77ee965f2b5c735aa45f882ca67ab4d4966ef1
Nickolas <prikense@hotmail.com>
2020-10-30 14:12:22 -0700

    mapa y tile data, sprites caballitos, cursor

---
+++ Juego estrategias/Assets/MapMananger.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapMananger : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap map;
+
+    [SerializeField]
+    private List<tiledatascript> tileDatas;
+
+    private Dictionary<TileBase, tiledatascript> dataFromTiles;
+
+    private void Awake()
+    {
+        dataFromTiles = new Dictionary<TileBase, tiledatascript>();
+
+        foreach (var tiledatascript in tileDatas)
+        {
+            foreach (var tile in tiledatascript.tiles)
+            {
+                dataFromTiles.Add(tile, tiledatascript);
+
+                
+            }
+        }
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPosition = map.WorldToCell(mousePosition);
+
+            TileBase clickedTile = map.GetTile(gridPosition);
+
+            int crossCost2 =  dataFromTiles[clickedTile].crossCost;
+
+            print("At position " + gridPosition + " There is a " + clickedTile + "cost of crossing is "+ crossCost2);
+        }
+    }
+
+    public int GetTilecrossCost(Vector2 wordlPosition)
+    {
+        Vector3Int gridPosition = map.WorldToCell(wordlPosition);
+
+        TileBase tile = map.GetTile(gridPosition);
+
+        if (tile == null){return 0;}
+        else{
+        int crossCost2 =  dataFromTiles[tile].crossCost;
+        return crossCost2;
+        }
+    }
+}
---
+++ Juego estrategias/Assets/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 7;
+    [SerializeField]
+    private Transform movePoint;
+    [SerializeField]
+    private LayerMask obstacleMask;
+
+    void Start()
+    {
+        movePoint.parent = null; // Detach partent
+    }
+
+    void Update()
+    {
+        float movementAmout = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, movementAmout);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            if (Input.GetAxisRaw("Horizontal") < 0f)
+            {
+                Move(new Vector3(-1, 0, 0));
+            }
+            if (Input.GetAxisRaw("Vertical") < 0f)
+            {
+                Move(new Vector3(0, -1, 0));
+            }
+            if (Input.GetAxisRaw("Horizontal") > 0f)
+            {
+                Move(new Vector3(1, 0, 0));
+            }
+            if (Input.GetAxisRaw("Vertical") > 0f)
+            {
+                Move(new Vector3(0, 1, 0));
+            }
+        }
+    }
+
+    private void Move(Vector3 direction)
+    {
+        Vector3 newPosition = movePoint.position + direction;
+        if (!Physics2D.OverlapCircle(newPosition, 0.2f, obstacleMask))
+        {
+            movePoint.position = newPosition;
+        }
+    }
+}
